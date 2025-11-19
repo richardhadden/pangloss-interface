8601cae9ce30eee70b1945731efe0eb9b794c7cf
Richard Hadden <richard.hadden@oeaw.ac.at>
2025-11-19 15:15:30 +0100

    restored

--- pangloss_interface/src/components/ControlBar.tsx
+++ pangloss_interface/src/components/ControlBar.tsx
@@ -3,7 +3,7 @@ import { BiRegularPlus } from "solid-icons/bi";
 import { IoSaveSharp } from "solid-icons/io";
 import { JSX, JSXElement } from "solid-js";
 import { Portal, Show } from "solid-js/web";
-import { useTranslation, type TranslationKey } from "~/contexts/translation";
+import { useTranslation, type TranslationKey } from "../contexts/translation";
 
 type ControlBarProps = {
   modelType?: BaseNodeTypes;
@@ -14,18 +14,19 @@ type ControlBarProps = {
   leftContent?: JSXElement;
   centreContent?: JSX.Element;
   statusContent?: JSX.Element;
+  rightButtons?: JSX.Element;
 };
 
 export default function ControlBar(props: ControlBarProps) {
   const [_, { t }] = useTranslation();
   return (
     <Portal mount={document.getElementById("controlBar")!}>
-      <div class="w-full h-16 rounded-b-xs flex flex-between *:first:rounded-bl-sm group">
+      <div class="flex-between group flex h-16 w-full rounded-b-xs *:first:rounded-bl-sm">
         <Show when={props.statusContent}>{props.statusContent}</Show>
         <Show when={props.modelType}>
           <div
             id="controlBarModelType"
-            class="h-16 first-of-type:rounded-bl-xs flex items-center bg-slate-800/80 backdrop-blur-2xl px-4 uppercase font-semibold text-slate-100 text-sm select-none text-shadow-2xs text-shadow-blue-600 group-hover:shadow-2xl group-hover:shadow-slate-800/40"
+            class="text-shadow-2xs text-shadow-blue-600 flex h-16 items-center bg-slate-800/80 px-4 text-sm font-semibold text-slate-100 uppercase backdrop-blur-2xl select-none group-hover:shadow-2xl group-hover:shadow-slate-800/40 first-of-type:rounded-bl-xs"
           >
             <Show
               when={props.modelTypeNumber === "plural"}
@@ -41,18 +42,18 @@ export default function ControlBar(props: ControlBarProps) {
         <div id="controlBarCentre" class="flex-1">
           {props.centreContent}
         </div>
-        <div id="controlBarRight"></div>
-        <div id="controlBarButtons">
+        <div id="controlBarButtons" class="flex">
+          {props.rightButtons}
           <Show when={props.addFunction}>
             <button
               onClick={props.addFunction}
               id="controlBarAddButton"
-              class="last:rounded-br-xs aspect-square h-full active:shadow-inner bg-green-700/90 hover:bg-green-800/90 active:bg-green-600/90 hover:shadow-xl hover:shadow-green-100/30 shadow-g group/button flex justify-center items-center cursor-pointer"
+              class="shadow-g group/button flex aspect-square h-full cursor-pointer items-center justify-center bg-green-700/90 last:rounded-br-xs hover:bg-green-800/90 hover:shadow-xl hover:shadow-green-100/30 active:bg-green-600/90 active:shadow-inner"
             >
               <BiRegularPlus
                 color="white"
                 size={30}
-                class="group-active/button:scale-90 group-hover/button:scale-110"
+                class="group-hover/button:scale-110 group-active/button:scale-90"
               />
             </button>
           </Show>
@@ -60,12 +61,12 @@ export default function ControlBar(props: ControlBarProps) {
             <button
               onClick={props.saveFunction}
               id="controlBarSaveButton"
-              class="last:rounded-br-xs aspect-square h-full active:shadow-inner bg-orange-500/70 hover:bg-orange-500/70 active:bg-orange-400/70  active:shadow-orange-500/30 group-hover:shadow-md group-hover:shadow-300/40 hover:shadow-orange-300/40 shadow-g group/button flex justify-center items-center cursor-pointer"
+              class="group-hover:shadow-300/40 shadow-g group/button flex aspect-square h-full cursor-pointer items-center justify-center bg-orange-500/70 group-hover:shadow-md last:rounded-br-xs hover:bg-orange-500/70 hover:shadow-orange-300/40 active:bg-orange-400/70 active:shadow-inner active:shadow-orange-500/30"
             >
               <IoSaveSharp
                 color="white"
                 size={20}
-                class="group-active/button:scale-95 group-hover/button:scale-105"
+                class="group-hover/button:scale-105 group-active/button:scale-95"
               />
             </button>
           </Show>
--- pangloss_interface/src/routes/[modelType]/new.tsx
+++ pangloss_interface/src/routes/[modelType]/new.tsx
@@ -1,6 +1,6 @@
 import { BaseNodeTypes } from "../../../.model-configs/model-typescript";
 import { useParams } from "@solidjs/router";
-import { createEffect, Show } from "solid-js";
+import { createEffect, onCleanup, onMount, Show } from "solid-js";
 import { createStore, produce, reconcile, unwrap } from "solid-js/store";
 import ControlBar from "~/components/ControlBar";
 import { BaseForm } from "~/components/form/BaseForm";
@@ -9,15 +9,36 @@ import { createBlankObject } from "~/utils/createBlankObject";
 import { RequireLogin } from "~/contexts/users";
 import { Title } from "@solidjs/meta";
 import { TranslationKey, useTranslation } from "~/contexts/translation";
+import { createUndoHistory } from "@solid-primitives/history";
+
+import { IoArrowRedoSharp, IoArrowUndoSharp } from "solid-icons/io";
+import colors from "tailwindcss/colors";
+import { bindKeyCombo, unbindKeyCombo } from "@rwh/keystrokes";
 
 export default function CreateObject() {
   const params = useParams<{ modelType: BaseNodeTypes }>();
 
   const [newFormState, setNewFormState] = createStore(
-    createBlankObject(params.modelType, false),
+    createBlankObject(params.modelType, false)
   );
 
+  const history = createUndoHistory(() => {
+    // track and clone the whole state
+    const copy = JSON.parse(JSON.stringify(newFormState));
+    // reconcile the state back to the tracked value
+    return () => setNewFormState(reconcile(copy));
+  });
+
   const [lang, { t }] = useTranslation();
+
+  onMount(() => {
+    bindKeyCombo("meta > z", history.undo);
+    bindKeyCombo("meta + shift > z", history.redo);
+    onCleanup(() => {
+      unbindKeyCombo("meta > z", history.undo);
+      unbindKeyCombo("meta + shift > z", history.redo);
+    });
+  });
 
   // Navigating from one `new` page to another directly does not
   // unmount the component, so the default store value is not changed;
@@ -64,14 +85,41 @@ export default function CreateObject() {
             </div>
           </Show>
         }
+        rightButtons={
+          <>
+            <button
+              disabled={!history.canUndo()}
+              onclick={() => history.undo()}
+              class="group-hover:shadow-300/40 shadow-g group/button flex aspect-square h-full cursor-pointer items-center justify-center bg-slate-500/70 group-hover:shadow-md last:rounded-br-xs hover:bg-slate-500/70 hover:shadow-slate-300/40 active:bg-slate-400/70 active:shadow-inner active:shadow-slate-500/30 disabled:cursor-not-allowed disabled:bg-slate-500/50 disabled:hover:bg-slate-500/50 disabled:hover:shadow-none"
+            >
+              <IoArrowUndoSharp
+                color={history.canUndo() ? "white" : colors.slate["300"]}
+                size={20}
+              />
+            </button>
+            <button
+              disabled={!history.canRedo()}
+              onclick={() => history.redo()}
+              class="group-hover:shadow-300/40 shadow-g group/button flex aspect-square h-full cursor-pointer items-center justify-center bg-slate-500/70 group-hover:shadow-md last:rounded-br-xs hover:bg-slate-500/70 hover:shadow-slate-300/40 active:bg-slate-400/70 active:shadow-inner active:shadow-slate-500/30 disabled:cursor-not-allowed disabled:bg-slate-500/50 disabled:hover:bg-slate-500/50 disabled:hover:shadow-none"
+            >
+              <IoArrowRedoSharp
+                color={history.canRedo() ? "white" : colors.slate["300"]}
+                size={20}
+              />
+            </button>
+          </>
+        }
       />
 
       <div class="py-32 pr-16 pl-32">
         {/*<button onclick={() => console.log(unwrap(newFormState))}>
           CONSOLE LOG FORM STATE
         </button>*/}
-        <Show when={params.modelType} keyed>
-          {(a) => (
+        <Show when={params.modelType}>
+          {/* Use this callback form to ensure form is rerendered when 
+              route changes
+          */}
+          {(_) => (
             <BaseForm
               formFor={params.modelType}
               baseFormState={newFormState}
